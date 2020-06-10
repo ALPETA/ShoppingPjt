fa053e60b714e65f52a038927c9117806485e7b7
LHcom <wgs04139@naver.com>
2020-06-10 22:02:30 +0900

    retry

--- ComputerShop/src/main/java/com/hardCarry/shopping/HomeController.java
+++ ComputerShop/src/main/java/com/hardCarry/shopping/HomeController.java
@@ -1,16 +1,8 @@
 package com.hardCarry.shopping;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@Slf4j
 public class HomeController {
-
-
-	
 
 }
